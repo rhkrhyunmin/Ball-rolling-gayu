31338432a14fff3e0448251ff5a752bd6a37ac69
rhkrhyunmin <rhkrhyunmin@gmail.com>
2023-05-14 23:54:41 +0900

    5.14

--- NewEngineProject/Assets/02.Scrpis/Cannon/CannonMove.cs
+++ NewEngineProject/Assets/02.Scrpis/Cannon/CannonMove.cs
@@ -4,25 +4,40 @@ using UnityEngine;
 
 public class CannonMove : MonoBehaviour
 {
-    public float rotationSpeed = 100f;
-    public float maxUpAngle = 45f; // 최대 위쪽 회전 각도 (degree)
-    public float maxDownAngle = -10f; // 최대 아래쪽 회전 각도 (degree)
+    public GameObject projectilePrefab;    // 발사체 프리팹
+    public Transform spawnPoint;           // 발사 위치
+    public float fireForce = 500.0f;       // 발사 속도
+    public float rotateSpeed = 50.0f;      // 회전 속도
+    public float minAngle = -80.0f;        // 최소 각도
+    public float maxAngle = 80.0f;         // 최대 각도
 
-    private float currentAngle = 0f;
+    private float currentAngle;            // 현재 각도
+
+    void Start()
+    {
+        currentAngle = 0.0f;    // 최초 각도는 0
+    }
 
     void Update()
     {
-        // 방향키 입력을 감지하여 대포의 회전 값을 조정합니다.
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        // 좌우 회전
+        float horizontal = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horizontal * rotateSpeed * Time.deltaTime);
 
-        // 현재 회전 각도를 업데이트합니다.
-        currentAngle += verticalInput * rotationSpeed * Time.deltaTime;
+        // 상하 회전
+        float vertical = Input.GetAxis("Vertical");
+        float targetAngle = currentAngle + vertical * rotateSpeed * Time.deltaTime;
+        targetAngle = Mathf.Clamp(targetAngle, minAngle, maxAngle);
 
-        // 회전 각도를 일정 범위 내에서 제한합니다.
-        currentAngle = Mathf.Clamp(currentAngle, maxDownAngle, maxUpAngle);
+        transform.localRotation = Quaternion.Euler(-targetAngle, transform.localRotation.eulerAngles.y, 0);
+        currentAngle = targetAngle;
 
-        // Cannon 게임 오브젝트의 transform을 사용하여 회전합니다.
-        transform.GetChild(0).localRotation = Quaternion.Euler(currentAngle, horizontalInput * rotationSpeed * Time.deltaTime, 0f);
+        // 발사
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject projectile = Instantiate(projectilePrefab, spawnPoint.position, Quaternion.identity);
+            Rigidbody rb = projectile.GetComponent<Rigidbody>();
+            rb.AddForce(transform.forward * fireForce);
+        }
     }
 }
