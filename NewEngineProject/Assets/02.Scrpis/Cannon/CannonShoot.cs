329cca7921a32ff9e8186a192abe49b5bbfd2b19
rhkrhyunmin <rhkrhyunmin@gmail.com>
2023-05-17 01:23:55 +0900

    5.17

--- NewEngineProject/Assets/02.Scrpis/Cannon/CannonShoot.cs
+++ NewEngineProject/Assets/02.Scrpis/Cannon/CannonShoot.cs
@@ -5,19 +5,54 @@ using UnityEngine;
 public class CannonShoot : MonoBehaviour
 {
     public GameObject ballPrefab;
-    public float forceMagnitude = 10f; // 기본 힘의 크기
+    public float minForceMagnitude = 10f; // 최소 힘의 크기
     public float maxForceMagnitude = 50f; // 최대 힘의 크기
+    public float chargeRate = 1f; // 차징 속도
+
+    private float currentForceMagnitude = 0f; // 현재 힘의 크기
+    private bool isCharging = false; // 차징 중인지 여부
+    private float chargeStartTime = 0f; // 차징 시작 시간
+    public Transform cannonExit; // 대포의 입구 위치
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            float force = Mathf.Clamp(forceMagnitude, 0f, maxForceMagnitude);
-            Vector3 forceVector = new Vector3(0f, 0f, force);
-
-            GameObject ballInstance = Instantiate(ballPrefab, transform.position, Quaternion.identity);
-            Rigidbody ballRb = ballInstance.GetComponent<Rigidbody>();
-            ballRb.AddForce(forceVector, ForceMode.Impulse);
+            StartCharging();
         }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Fire();
+        }
+
+        if (isCharging)
+        {
+            UpdateCharge();
+        }
+    }
+
+    private void StartCharging()
+    {
+        isCharging = true;
+        chargeStartTime = Time.time;
+    }
+
+    private void Fire()
+    {
+        isCharging = false;
+        float force = Mathf.Lerp(minForceMagnitude, maxForceMagnitude, currentForceMagnitude);
+        Vector3 forceVector = cannonExit.forward * force;
+
+        GameObject ballInstance = Instantiate(ballPrefab, cannonExit.position, Quaternion.identity);
+        Rigidbody ballRb = ballInstance.GetComponent<Rigidbody>();
+        ballRb.AddForce(forceVector, ForceMode.Impulse);
+
+        currentForceMagnitude = 0f;
+    }
+
+    private void UpdateCharge()
+    {
+        float chargeTime = Time.time - chargeStartTime;
+        currentForceMagnitude = Mathf.Clamp(chargeTime * chargeRate, 0f, 1f);
     }
 }
