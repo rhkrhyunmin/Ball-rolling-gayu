981eac431c225c1de8a16a7254660d448997e138
rhkrhyunmin <rhkrhyunmin@gmail.com>
2023-05-17 01:04:56 +0900

    5.17

--- NewEngineProject/Assets/02.Scrpis/Cannon/CannonShoot.cs
+++ NewEngineProject/Assets/02.Scrpis/Cannon/CannonShoot.cs
@@ -4,29 +4,20 @@ using UnityEngine;
 
 public class CannonShoot : MonoBehaviour
 {
-    public Rigidbody ballPrefab;
-    public float forceMultiplier = 1f;
+    public GameObject ballPrefab;
+    public float forceMagnitude = 10f; // 기본 힘의 크기
+    public float maxForceMagnitude = 50f; // 최대 힘의 크기
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            // 프리팹 복제
-            Rigidbody ballInstance = Instantiate(ballPrefab, transform.position, Quaternion.identity);
+            float force = Mathf.Clamp(forceMagnitude, 0f, maxForceMagnitude);
+            Vector3 forceVector = new Vector3(0f, 0f, force);
 
-            // 힘의 크기 계산
-            float forceMagnitude = CalculateForceMagnitude();
-
-            // 힘 적용
-            Vector3 force = new Vector3(0f, 0f, forceMagnitude);
-            ballInstance.AddForce(force);
+            GameObject ballInstance = Instantiate(ballPrefab, transform.position, Quaternion.identity);
+            Rigidbody ballRb = ballInstance.GetComponent<Rigidbody>();
+            ballRb.AddForce(forceVector, ForceMode.Impulse);
         }
-    }
-
-    private float CalculateForceMagnitude()
-    {
-        // 힘의 크기를 계산하는 로직을 추가합니다.
-        // 예를 들어, 키를 누르는 시간에 따라 힘의 크기를 변화시킬 수 있습니다.
-        return forceMultiplier;  // 힘의 크기에 곱할 수를 반환합니다.
     }
 }
