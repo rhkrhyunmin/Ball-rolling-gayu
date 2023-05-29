b6217694f2501c9d2048af0ee75cf7878da3a807
rhkrhyunmin <rhkrhyunmin@gmail.com>
2023-05-30 00:29:08 +0900

    5.30

--- NewEngineProject/Assets/02.Scrpis/Clear/ClearBallMove.cs
+++ NewEngineProject/Assets/02.Scrpis/Clear/ClearBallMove.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class ClearBallMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform target;
+    public float movementSpeed = 5f;
+    public float stoppingDistance = 1f; // 도착할 거리
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (target != null)
+        {
+            // 적을 향해 이동하는 방향 계산
+            Vector3 direction = (target.position - transform.position).normalized;
+
+            float distance = Vector3.Distance(transform.position, target.position);
+
+            // 적과의 거리가 도착할 거리보다 크면 이동
+            if (distance > stoppingDistance)
+            {
+                // 계산된 방향으로 이동
+                transform.Translate(direction * movementSpeed * Time.deltaTime);
+            }
+        }
     }
 }
