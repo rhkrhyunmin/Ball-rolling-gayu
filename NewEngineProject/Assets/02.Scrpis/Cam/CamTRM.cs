75407229ac297f62bb100f7d108765df09a77cbc
rhkrhyunmin <rhkrhyunmin@gmail.com>
2023-06-06 23:43:14 +0900

    6.6

--- NewEngineProject/Assets/02.Scrpis/Cam/CamTRM.cs
+++ NewEngineProject/Assets/02.Scrpis/Cam/CamTRM.cs
@@ -6,6 +6,7 @@ public class CamTRM : MonoBehaviour
 {
     [SerializeField] private Transform EndTrm; //이걸 할려면 End값을 어디에다가 해야하는지 정해야하는데 아직 못정함 
     [SerializeField] private float Movespeed = 8;
+    [SerializeField] private float RotationSpeed = 5f;
 
     private float StartX, EndX;
 
@@ -20,12 +21,16 @@ public class CamTRM : MonoBehaviour
     void Update()
     {
         float xMove = Input.GetAxisRaw("Horizontal");
+        float yMove = Input.GetAxisRaw("Vertical");
 
-        float YMove = Input.GetAxisRaw("Vertical");
-
-        float Data = xMove * Time.deltaTime * Movespeed;
-        Vector3 xPos = transform.position + new Vector3(Data, 0, 0);
+        // 이동 로직
+        float xData = xMove * Time.deltaTime * Movespeed;
+        Vector3 xPos = transform.position + new Vector3(xData, 0, 0);
         xPos.x = Mathf.Clamp(xPos.x, StartX, EndX);
         transform.position = xPos;
+
+        // 회전 로직
+        float rotationAmount = -xMove * Time.deltaTime * RotationSpeed;
+        transform.Rotate(0, 0, rotationAmount);
     }
 }
