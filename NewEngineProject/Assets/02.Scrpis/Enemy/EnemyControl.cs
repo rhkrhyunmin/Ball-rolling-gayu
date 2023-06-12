6f866e23e02b18284384b6835a41a15ec4bba934
rhkrhyunmin <rhkrhyunmin@gmail.com>
2023-06-13 00:39:28 +0900

    6.13

--- NewEngineProject/Assets/02.Scrpis/Enemy/EnemyControl.cs
+++ NewEngineProject/Assets/02.Scrpis/Enemy/EnemyControl.cs
@@ -5,24 +5,27 @@ using UnityEngine;
 public class EnemyControl : MonoBehaviour
 {
     public float detectionDistance = 10f;
+    public float attackDistance = 2f;
     public float movementSpeed = 2f;
+    public float currentHp = 0;
+    public float MaxHp = 10;
     public Animator animator;
+
+    Rigidbody rb;
+    BallHp ballHp;
 
     [SerializeField]
     private BallMove playerController;
     private bool isPlayerDetected = false;
-
     private bool isIdle = true;
+    private bool isAttacking = false;
+    private float attackTimer = 0f;
+    public float attackCooldown = 2f;
 
     private void Start()
     {
-        // 플레이어 컨트롤러 스크립트를 가지고 있는 오브젝트에서 해당 컴포넌트를 가져옵니다.
-       
-
-        if (playerController == null)
-        {
-            
-        }
+        rb = GetComponent<Rigidbody>();
+        ballHp = GetComponent<BallHp>();
     }
 
     public void FindPlayer(BallMove ball)
@@ -37,32 +40,64 @@ public class EnemyControl : MonoBehaviour
             return;
         }
 
-        // 플레이어와의 거리를 계산합니다.
         float distanceToPlayer = Vector3.Distance(transform.position, playerController.transform.position);
-        Debug.Log(distanceToPlayer);
-        
 
-        // 거리에 따라 애니메이션을 변경하고 이동합니다.
-        if (distanceToPlayer <= detectionDistance)
+        if (isIdle)
         {
-            animator.SetBool("IsWalk", true);
-            isPlayerDetected = true;
-            isIdle = false;
-
+            // IDLE 애니메이션이 실행 중인 경우
+            if (distanceToPlayer <= detectionDistance)
+            {
+                // 제한된 거리 안에 플레이어가 들어오면 Walk 애니메이션 실행
+                animator.SetBool("IsWalk", true);
+                isPlayerDetected = true;
+                isIdle = false;
+            }
         }
         else
         {
-            animator.SetBool("IsWalk", false);
-            isPlayerDetected = false;
-            isIdle = true;
+            // Walk 애니메이션 실행 중인 경우
+            if (distanceToPlayer > detectionDistance)
+            {
+                // 제한된 거리 밖으로 나가면 IDLE 애니메이션 실행
+                animator.SetBool("IsWalk", false);
+                isPlayerDetected = false;
+                isIdle = true;
+            }
         }
 
-        // 플레이어를 따라 이동합니다.
         if (isPlayerDetected)
         {
             Vector3 direction = (playerController.transform.position - transform.position).normalized;
             transform.Translate(direction * movementSpeed * Time.deltaTime);
             transform.LookAt(playerController.transform.position);
+
+            if (distanceToPlayer <= attackDistance && !isAttacking)
+            {
+                // 공격 범위 안에 플레이어가 있고, 공격 중이 아닌 경우 공격 애니메이션 실행
+                Attack();
+            }
         }
+
+        if (isAttacking)
+        {
+            attackTimer += Time.deltaTime;
+            if (attackTimer >= attackCooldown)
+            {
+                isAttacking = false;
+                animator.SetBool("IsAttack", false);
+                animator.SetBool("IsWalk", true);
+                isPlayerDetected = true;
+                attackTimer = 0f;
+            }
+        }
+    }
+
+    private void Attack()
+    {
+        isAttacking = true;
+        animator.SetBool("IsAttack", true);
+        animator.SetBool("IsWalk", false);
+        isPlayerDetected = false;
+        transform.LookAt(playerController.transform.position);
     }
 }
