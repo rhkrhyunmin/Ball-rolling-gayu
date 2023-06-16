65c7512a6bcfc8125f4d315aeaa33342f0c7a165
rhkrhyunmin <rhkrhyunmin@gmail.com>
2023-06-16 16:46:50 +0900

    6.16

--- NewEngineProject/Assets/02.Scrpis/Tutorial/tutorial.cs
+++ NewEngineProject/Assets/02.Scrpis/Tutorial/tutorial.cs
@@ -8,16 +8,35 @@ public class tutorial : MonoBehaviour
 {
     public BoxCollider[] gameObjects; // 생성된 GameObject 배열
     public TextMeshProUGUI[] textMeshes; // 생성된 TextMeshProUGUI 배열
+    public TextMeshProUGUI[] FirstText;
     private bool isGamePaused; // 게임 일시정지 여부
 
     private int currentIndex = 0;
+    private int previousIndex = -1;
     [SerializeField]
     private LayerMask layer;
 
+    public float timeInterval = 5f; // 5초마다 다음 텍스트로 전환
+
+    private float timer = 0f;
+    private bool isNextTextRunning = false; // NextText() 함수 실행 여부
 
     private void Update()
     {
         ObjCheck();
+
+        if (!isGamePaused)
+        {
+            timer += Time.deltaTime;
+            if (timer >= timeInterval)
+            {
+                timer = 0f;
+                if (!isNextTextRunning)
+                {
+                    NextText();
+                }
+            }
+        }
 
         // "F" 키를 누르면 게임 재개
         if (Input.GetKeyDown(KeyCode.F))
@@ -30,12 +49,58 @@ public class tutorial : MonoBehaviour
     private void ObjCheck()
     {
         bool check = Physics.CheckBox(gameObjects[currentIndex].transform.position + gameObjects[currentIndex].center, gameObjects[currentIndex].size, Quaternion.identity, layer);
-        if(check)
+        if (check)
         {
             textMeshes[currentIndex].gameObject.SetActive(false);
             currentIndex++;
+            if (currentIndex >= textMeshes.Length)
+            {
+                currentIndex = 0;
+            }
             textMeshes[currentIndex].gameObject.SetActive(true);
         }
+    }
+
+    private void NextText()
+    {
+        int newIndex = GetNextIndex();
+
+        if (newIndex != 0)
+        {
+            FirstText[currentIndex].gameObject.SetActive(false);
+            currentIndex = (currentIndex + 1) % FirstText.Length;
+            FirstText[currentIndex].gameObject.SetActive(true);
+        }
+        else
+        {
+            FirstText[currentIndex].gameObject.SetActive(false); // 현재 텍스트를 비활성화
+        }
+    }
+
+    private int GetNextIndex()
+    {
+        int count = FirstText.Length;
+        if (count <= 1)
+        {
+            return 0; // 배열의 크기가 1이하인 경우에는 항상 0번째 인덱스를 반환
+        }
+
+        int attempts = 0;
+        int randomIndex = UnityEngine.Random.Range(0, count); // 랜덤한 인덱스에서 시작
+        while (attempts < count)
+        {
+            if (randomIndex != currentIndex && randomIndex != previousIndex)
+            {
+                return randomIndex; // 이전 인덱스와 현재 인덱스가 아닌 경우, 해당 인덱스 반환
+            }
+            else
+            {
+                randomIndex = (randomIndex + 1) % count; // 다음 인덱스로 이동
+                attempts++;
+            }
+        }
+
+        return -1; // 적절한 인덱스를 찾지 못한 경우 -1 반환
     }
 
     private void OnDrawGizmos()
