2ba258585137ca6efed1ae89dd47a7c52e1795a9
rhkrhyunmin <rhkrhyunmin@gmail.com>
2023-06-17 12:33:20 +0900

    6.17

--- NewEngineProject/Assets/02.Scrpis/Ball/BallAttack.cs
+++ NewEngineProject/Assets/02.Scrpis/Ball/BallAttack.cs
@@ -4,17 +4,33 @@ using UnityEngine;
 
 public class BallAttack : MonoBehaviour
 {
+    public float dashForce = 10f;          // 돌진 힘
     public float dashDuration = 0.5f;      // 돌진 지속 시간
     public float dashCooldown = 1f;        // 돌진 쿨다운 시간
     public float dashDamage = 10f;         // 돌진으로 입힐 데미지
     public float dashKnockbackForce = 5f;  // 돌진으로 적을 튕겨낼 힘
+    public float detectionRadius = 5f;      // 보스 감지 반경
+    public float upwardForce = 5f;       // 위로 가할 힘
+
+    private Rigidbody playerRigidbody;  // 플레이어 Rigidbody 컴포넌트
 
     private bool isDashing = false;        // 돌진 중인지 여부
     private float dashTimer = 0f;          // 돌진 타이머
     private float dashCooldownTimer = 0f;  // 돌진 쿨다운 타이머
+    public GameObject bossObject;  // 보스 게임 오브젝트
+
+
+    private void Start()
+    {
+        // 보스를 찾아서 Transform을 가져옴
+        bossObject = GameObject.FindGameObjectWithTag("Boss");
+
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
 
     private void Update()
     {
+        // 돌진 쿨다운 갱신
         dashCooldownTimer -= Time.deltaTime;
 
         // 스페이스 바를 누르고 돌진 쿨다운이 0보다 작거나 같을 때만 돌진 가능
@@ -34,19 +50,26 @@ public class BallAttack : MonoBehaviour
                 EndDash();
             }
         }
+        else if (!isDashing && dashCooldownTimer <= 0f && Input.GetKey(KeyCode.Space))
+        {
+            // 돌진 쿨다운이 끝났고, Space 키가 여전히 눌려있으면 돌진 유지
+            StartDash();
+        }
     }
 
     private void StartDash()
     {
-        // 돌진 상태로 변경
         isDashing = true;
         dashTimer = 0f;
 
-        // 돌진 방향 설정 (여기서는 플레이어가 바라보는 방향)
-        Vector3 dashDirection = transform.forward;
+        // 돌진 방향 설정 (보스 위치를 향하도록)
+        Vector3 dashDirection = (bossObject.transform.position - transform.position).normalized;
 
         // 돌진 힘 적용
-        GetComponent<Rigidbody>().AddForce(dashDirection * dashForce, ForceMode.Impulse);
+        playerRigidbody.AddForce(dashDirection * dashForce, ForceMode.Impulse);
+
+        // 위로 가하는 힘 적용
+        playerRigidbody.AddForce(Vector3.up * upwardForce, ForceMode.Impulse);
     }
 
     private void EndDash()
