9e7a14337f35c481258e97442e44b7d106a93384
rhkrhyunmin <rhkrhyunmin@gmail.com>
2024-05-29 23:56:40 +0900

    수정

--- NewEngineProject/Assets/02.Scrpis/UI/Navegation.cs
+++ NewEngineProject/Assets/02.Scrpis/UI/Navegation.cs
@@ -7,7 +7,7 @@ public class Navegation : MonoBehaviour
 {
     public Transform Destination; // 목적지 Transform
     public Slider progressSlider; // 슬라이더 UI
-    public float maxDistance = 150f; // 최대 거리
+    public float maxDistance = 2000f; // 최대 거리
     private Transform playerTransform;
 
     void Update()
@@ -30,8 +30,8 @@ public class Navegation : MonoBehaviour
     {
         float distance = Vector3.Distance(playerTransform.position, Destination.position); // 플레이어와 목적지 사이의 거리 계산
         float progress = 1 - Mathf.Clamp01(distance / maxDistance); // 슬라이더 값 계산 (0 ~ 1 사이)
-        Debug.Log(distance);
-        Debug.Log(progress);
+        Debug.Log("Distance: " + distance);
+        Debug.Log("Progress: " + progress);
         progressSlider.value = progress; // 슬라이더 값 업데이트
     }
 }
