bb906bb9cb76d8721170f3d3419031ed9791dd78
rhkrhyunmin <rhkrhyunmin@gmail.com>
2024-06-01 20:16:42 +0900

    수정

--- NewEngineProject/Assets/02.Scrpis/Trap/TrapBallSpawn.cs
+++ NewEngineProject/Assets/02.Scrpis/Trap/TrapBallSpawn.cs
@@ -6,6 +6,7 @@ public class TrapBallSpawn : MonoBehaviour
 {
     public string poolTag = "Trap";
     private float spawnRandomTime;
+    public List<Transform> spawnPoints; // TrapBall이 스폰될 위치들을 저장하는 리스트
 
     private void Start()
     {
@@ -26,7 +27,6 @@ public class TrapBallSpawn : MonoBehaviour
     {
         yield return new WaitForSeconds(28f);
         PoolManager.Instance.ReturnObject(trapBall);
-        
     }
 
     private void SpawnTrapBall()
@@ -34,8 +34,10 @@ public class TrapBallSpawn : MonoBehaviour
         GameObject trapBall = PoolManager.Instance.GetObject(poolTag);
         if (trapBall != null)
         {
-            trapBall.transform.position = transform.position;
-            trapBall.transform.rotation = transform.rotation;
+            // 무작위로 스폰 위치를 선택합니다.
+            Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Count)];
+            trapBall.transform.position = spawnPoint.position;
+            trapBall.transform.rotation = spawnPoint.rotation;
             StartCoroutine(DisableTrapBall(trapBall));
         }
     }
