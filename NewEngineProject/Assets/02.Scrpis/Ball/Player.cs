d1bdf37d81ce277d15f1dca22eee2e6bf2efa8ec
rhkrhyunmin <rhkrhyunmin@gmail.com>
2024-07-07 18:27:10 +0900

    맵수정

--- NewEngineProject/Assets/02.Scrpis/Ball/Player.cs
+++ NewEngineProject/Assets/02.Scrpis/Ball/Player.cs
@@ -37,29 +37,26 @@ public class Player : MonoBehaviour
                 ballSO.moveSpeed -= Time.deltaTime;
             }
 
-            ballSO.moveSpeed = Mathf.Clamp(ballSO.moveSpeed, 0, 15);
+            
             Vector3 normalizedDir = dir.normalized;
             Vector3 force = normalizedDir * ballSO.moveSpeed;
             rigid.AddForce(force, ForceMode.Force);
 
             float velocityMagnitude = rigid.velocity.magnitude;
+            velocityMagnitude = Mathf.Clamp(ballSO.moveSpeed, 0, 15);
             UIManager.Instance.speedGage.fillAmount = velocityMagnitude / 100;
 
-            if(ballSO.moveSpeed > 10)
+            if(velocityMagnitude > 10)
             {
-                UIManager.Instance.boostPack.fillAmount++;
+                UIManager.Instance.boostPack.fillAmount += 0.001f;
             }
 
-            if(UIManager.Instance.boostPack.fillAmount == 1)
-            {
-                isBoost = true;
-            }
-        }
-
-        if (canUseSpace && Input.GetKeyDown(KeyCode.Space))
-        {
-            StartCoroutine(SpaceCooldown());
             OnBoost();
+
+            if (isBoost == true)
+            {
+                UIManager.Instance.boostPack.fillAmount -= 0.01f;
+            }
         }
     }
 
@@ -104,10 +101,19 @@ public class Player : MonoBehaviour
 
     private void OnBoost()
     {
-        if (isBoost)
+        if (UIManager.Instance.boostPack.fillAmount == 1 && Input.GetKeyDown(KeyCode.F))
         {
+            isBoost = true;
+            Debug.Log(isBoost);
             //boostParticle.Play();
             StartCoroutine(BoostCo(3f));
+        }
+
+        else if(UIManager.Instance.boostPack.fillAmount == 0)
+        {
+            isBoost = false;
+            Debug.Log(isBoost);
+            //boostParticle.Stop();
         }
     }
 
@@ -115,13 +121,6 @@ public class Player : MonoBehaviour
     {
         rigid.AddForce(Vector3.forward * ballSO.moveSpeed * 3);
         yield return new WaitForSeconds(delay);
-        boostParticle.Stop();
-    }
-
-    private IEnumerator SpaceCooldown()
-    {
-        canUseSpace = false;
-        yield return new WaitForSeconds(ballSO.dashCooldown);
-        canUseSpace = true;
+        //boostParticle.Stop();
     }
 }
